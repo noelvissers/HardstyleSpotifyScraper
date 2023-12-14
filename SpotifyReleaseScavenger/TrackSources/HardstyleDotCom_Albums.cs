3fbf4ae727de0ec9f776097ae8814f90a36808c8
Noël Vissers <noel.vissers@prodrive-technologies.com>
2023-12-14 15:55:54 +0100

    Update for changing the way albums are searched

--- SpotifyReleaseScavenger/TrackSources/HardstyleDotCom_Albums.cs
+++ SpotifyReleaseScavenger/TrackSources/HardstyleDotCom_Albums.cs
@@ -41,55 +41,10 @@ namespace SpotifyReleaseScavenger.TrackSources
         {
           break;
         }
+
+        // TODO: Check albums
+
         albumsChecked++;
-
-        string albumData = GetStringBetween(node.InnerHtml, "href=\"", "\">");
-
-        HtmlDocument albumDetail = web.Load(albumData);
-        IEnumerable<HtmlNode> nodeAlbum = albumDetail.DocumentNode
-          .Descendants("td")
-          .Where(n => n.HasClass("text-1"));
-
-        var nodeAlbumTracks = nodeAlbum.Select(item => item.FirstChild).ToList();
-
-        long indexStart = 0;
-        foreach (var nodeAlbumTrack in nodeAlbumTracks)
-        {
-          string compare = GetStringBetween(nodeAlbumTrack.OuterHtml, "href=\"", "\"><b class");
-          int indexCompare = compare.LastIndexOf('/');
-          compare = compare.Substring(indexCompare + 1, compare.Length - indexCompare - 1);
-          long indexCurrent = long.Parse(compare);
-
-          if (indexStart == 0)
-          {
-            indexStart = indexCurrent;
-          }
-          else if (indexStart != indexCurrent)
-          {
-            continue;
-          }
-          indexStart++;
-
-          TrackData track = new TrackData();
-          int nodeElement = 0;
-
-          foreach (var nodeAlbumTrackChild in nodeAlbumTrack.ChildNodes)
-          {
-            nodeElement++;
-            if (nodeAlbumTrackChild.Name == "b" && nodeElement % 2 == 1)
-            {
-              track.Title = nodeAlbumTrackChild.InnerHtml;
-            }
-            else if (nodeAlbumTrackChild.Name == "span")
-            {
-              track.Artist = nodeAlbumTrackChild.InnerHtml;
-            }
-          }
-          track.Hash = Convert.ToBase64String(SHA256.HashData(Encoding.UTF8.GetBytes($"{track.Artist}{track.Title}")));
-
-          Console.WriteLine($"[Hardstyle.com | Albums] [{track.Hash}] {track.Artist} - {track.Title}");
-          trackData.Add(track);
-        }
       }
 
       Console.WriteLine($"[Hardstyle.com | Albums] Retrieved a total of {trackData.Count()} entries.");
